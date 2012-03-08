1e2cda478972e95c0b3e08ea668c1e3d93172ee6
Stephen Figgins <stephen.figgins@gmail.com>
2012-03-08 14:06:26 -0600

    covert project to distutils.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    version='0.0.1',
+    description='Simple weblog application for Django',
+    author='Stephen Figgins',
+    author_email='stephen.figgins@gmail.com',
+    name='django-coursecal',
+    url='https://github.com/sfiggins/django-coursecal',
+    packages=['coursecal'],
+    license='BSD License',
+    platforms=['OS Independent'],
+)
