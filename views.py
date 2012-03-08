e91c27e3099c799616b76c9ef2cb72cf4823a98d
Stephen Figgins <stephen.figgins@gmail.com>
2012-03-08 11:49:25 -0600

    change render_to_response to render

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.template import Template, Context
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from coursecal.models import Course, CourseEvent
 from datetime import date
 
@@ -21,10 +21,10 @@ def upcoming_event_list_print(eventlist):
 def event_calendar(request):
     event_list = upcoming_event_list().order_by('date', 'start_time')
 
-    return render_to_response('courses/calendar.html', 
+    return render(request, 'courses/calendar.html', 
         {'courseevents': event_list})
 
 def course_by_slug(request, slug_id):
     c = get_object_or_404(Course, slug=slug_id)
     c_event_list = CourseEvent.objects.filter(course__exact=c).filter(date__gte=date.today()).order_by('date')
-    return render_to_response('courses/detail.html', {'course': c, 'events': c_event_list})
+    return render(request, 'courses/detail.html', {'course': c, 'events': c_event_list})
